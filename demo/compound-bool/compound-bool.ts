a4e06e01c1b77bd8dc145cc7ff2a79769d8c802a
Danny Redden <dredden@allegion.com>
2025-02-27 17:06:31 -0500

    boolean logic & compound bools

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,4 +1,29 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+// NOT
+const writing = true;
+const reading = !writing;
+assert.equal(reading, false);
 
+
+// OR
+// pick 1 movie
+const rating = 9;
+const favoriteMovie = false;
+
+const suggestedMovie = rating > 8 || favoriteMovie;
+assert.equal(suggestedMovie, true);
+
+
+// AND
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal(isTeenager, true);
+
+// our package
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
