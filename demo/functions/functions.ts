e01e9925b0e22b635f72ab508ac9140f28c3696f
Danny Redden <dannyredden18@gmail.com>
2025-03-19 20:12:49 -0400

    functions :D

--- demo/functions/functions.ts
+++ demo/functions/functions.ts
@@ -13,4 +13,28 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/functions.html
 
+function sayHello() {
+    console.log("Hello");
+}
 
+sayHello();
+
+function sum(lhs, rhs) {
+    return lhs + rhs;
+}
+
+const answer = sum(2, 200);
+console.log(answer);
+
+const answer2 = sum(answer, 2);
+assert.equal(answer2, 204);
+
+
+const answer3 = sum(sum(1, 1), sum(2, 2));
+console.log(answer3);
+
+const lhs = sum(1, 1);
+const rhs = sum(2, 2);
+
+const answer4 = sum(lhs, rhs);
+console.log(answer4);
